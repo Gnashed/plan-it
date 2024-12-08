ada0d123fec957428cd41321383fb0a651e956ea
Tion Blackmon <tion.blackmon@outlook.com>
2024-12-08 16:22:22 -0600

    Added a new file in /api for grades. Created a promise that retrieves the grades data from the endpoint based on the firebaseKey (classroom_id) that is passed in. Still a WIP.

---
+++ src/app/grades/[firebaseKey]/page.jsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+// import AddButton from '../../../components/buttons/AddButton';
+import { getGrades } from '../../../api/gradesData';
+
+export default function GradeBook({ params }) {
+  const { firebaseKey } = params;
+
+  const [grades, setGrades] = useState([]);
+
+  useEffect(() => {
+    getGrades(firebaseKey).then(setGrades);
+  }, []);
+
+  return (
+    <div className="d-flex flex-column my-4 text-center">
+      <h1>Grades</h1>
+
+      <div className="d-flex flex-row flex-wrap justify-content-center my-4 gradebook-container mb-3">
+        {/* TODO: Grid setup for grade book */}
+        <div className="container text-center">
+          {/* Column headers */}
+          <div className="row mt-5">
+            <p className="col">Student</p>
+            <p className="col">Homework</p>
+            <p className="col">Classwork</p>
+            <p className="col">Quizzes</p>
+            <p className="col">Projects</p>
+            <p className="col">Exams</p>
+          </div>
+
+          {/* Student's grades */}
+          {grades.map((grade) => (
+            <div className="row student-grades">
+              {/* TODO: Figure out how to render the student name */}
+              <p className="col">{grade.student_id}</p>
+              <p className="col">--</p>
+              <p className="col">--</p>
+              <p className="col">--</p>
+              <p className="col">--</p>
+              <p className="col">--</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* <AddButton buttonProp="grades" /> */}
+    </div>
+  );
+}
+
+GradeBook.propTypes = {
+  params: PropTypes.objectOf({}).isRequired,
+};
--- src/app/grades/page.jsx
+++ src/app/grades/page.jsx
@@ -1,41 +1,61 @@
-import React from 'react';
-import AddButton from '../../components/buttons/AddButton';
+'use client';
+
+import { useState, useEffect } from 'react';
+// import AddButton from '../../components/buttons/AddButton';
+import Modal from 'react-bootstrap/Modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
+import { useAuth } from '../../utils/context/authContext';
+import { getClassrooms } from '../../api/classroomData';
 
 export default function GradeBook() {
+  // For the React Bootsterap modal.
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  const [classrooms, setClassrooms] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getClassrooms(user.uid).then(setClassrooms);
+  }, []);
+
   return (
-    <div className="d-flex flex-column my-4 text-center">
-      <h1>Grades</h1>
+    <div className="d-flex flex-row flex-wrap justify-content-center my-4 mb-3">
+      <Button variant="warning" onClick={handleShow} className="mt-3">
+        Select classroom
+      </Button>
 
-      <div className="d-flex flex-row flex-wrap justify-content-center my-4 gradebook-container mb-3">
-        <div className="row">
-          <h4 className="mt-5">Select classroom</h4>
-        </div>
+      <Modal show={show} onHide={handleClose} data-bs-theme="dark">
+        <Modal.Header>
+          <Modal.Title>Select classroom</Modal.Title>
+        </Modal.Header>
 
-        {/* TODO: Grid setup for grade book */}
-        <div className="container text-center">
-          {/* Column headers */}
-          <div className="row mt-5">
-            <p className="col">Student</p>
-            <p className="col">Homework</p>
-            <p className="col">Classwork</p>
-            <p className="col">Quizzes</p>
-            <p className="col">Projects</p>
-            <p className="col">Exams</p>
-          </div>
+        <Modal.Body>
+          {/* TODO: Dropdown goes here */}
+          <Dropdown data-bs-theme="dark">
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Select ...
+            </Dropdown.Toggle>
 
-          {/* Student's grades */}
-          <div className="row student-grades">
-            <p className="col">Student name</p>
-            <p className="col">--</p>
-            <p className="col">--</p>
-            <p className="col">--</p>
-            <p className="col">--</p>
-            <p className="col">--</p>
-          </div>
-        </div>
-      </div>
+            <Dropdown.Menu>
+              {classrooms.map((classroom) => (
+                <Dropdown.Item key={classroom.firebaseKey}>
+                  <Link href={`/grades/${classroom.firebaseKey}`}>{classroom.classroom_name}</Link>
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Modal.Body>
 
-      <AddButton buttonProp="grades" />
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }

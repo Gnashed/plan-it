a19f088d2462d18df35d09eaf42c6c1db16252e9
Tion Blackmon <tion.blackmon@outlook.com>
2024-12-09 19:20:21 -0600

    Now rendering the values for a few assignment categories. On this commit I can only render the total score from the assignment and not the average. This is still a WIP but I am very close to getting this to work

--- src/app/grades/[firebaseKey]/page.jsx
+++ src/app/grades/[firebaseKey]/page.jsx
@@ -11,7 +11,53 @@ export default function GradeBook({ params }) {
   const [grades, setGrades] = useState([]);
 
   useEffect(() => {
-    getGrades(firebaseKey).then(setGrades);
+    // getGrades(firebaseKey).then(setGrades);
+
+    getGrades(firebaseKey).then((gradesArray) => {
+      const copyOfGrades = []; // Need this to concat all filtered arrays that includes the new properties that holds the averages.
+      let homeworkAccumulator = 0;
+      let classworkAccumulator = 0;
+      let quizAccumulator = 0;
+
+      // Filter the results based on assignment category
+      const filterByHomework = gradesArray.filter((gradeObj) => gradeObj.assignment_category === 'Homework');
+      const filterByClasswork = gradesArray.filter((gradeObj) => gradeObj.assignment_category === 'Classwork');
+      const filterByQuizzes = gradesArray.filter((gradeObj) => gradeObj.assignment_category === 'Quiz');
+
+      console.warn('Grades for homework: ', filterByHomework);
+      console.warn('Grades for classwork: ', filterByClasswork);
+      console.warn('Grades for quizzes: ', filterByQuizzes);
+
+      // TODO: Loop through each filtered array to find the scores, add the scores, then render the averages.
+
+      filterByHomework.forEach((gradeObj) => {
+        if (gradeObj.score) {
+          homeworkAccumulator += gradeObj.score;
+          gradeObj.homework_average = homeworkAccumulator; // eslint-disable-line no-param-reassign
+          copyOfGrades.push(gradeObj);
+        }
+      });
+      filterByClasswork.forEach((gradeObj) => {
+        if (gradeObj.score) {
+          classworkAccumulator += gradeObj.score;
+          gradeObj.classwork_average = classworkAccumulator; // eslint-disable-line no-param-reassign
+          copyOfGrades.push(gradeObj);
+        }
+      });
+      filterByQuizzes.forEach((gradeObj) => {
+        if (gradeObj.score) {
+          quizAccumulator += gradeObj.score;
+          gradeObj.quiz_average = quizAccumulator; // eslint-disable-line no-param-reassign
+          copyOfGrades.push(gradeObj);
+        }
+      });
+
+      // console.warn("Homework score sum: ", homeworkAccumulator);
+      // console.warn("Classwork score sum: ", classworkAccumulator);
+      // console.warn("Quizzes score sum: ", quizAccumulator);
+
+      setGrades(copyOfGrades);
+    });
   }, []);
 
   return (
@@ -37,9 +83,9 @@ export default function GradeBook({ params }) {
               <p className="col">
                 {grade.student_first_name} {grade.student_last_name}
               </p>
-              <p className="col">--</p>
-              <p className="col">--</p>
-              <p className="col">--</p>
+              <p className="col">--{grade.homework_average}</p>
+              <p className="col">--{grade.classwork_average}</p>
+              <p className="col">--{grade.quiz_average}</p>
               <p className="col">--</p>
               <p className="col">--</p>
             </div>
